a52593271d15482a02a1f8f1d802357aa7a6a071
myl7 <myl.ustc@gmail.com>
2019-07-19 20:59:59 +0800

    Add tests for post views in cobra

--- backend/cobra_wrapper/tests.py
+++ backend/cobra_wrapper/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 
 from . import models as cobra_models
@@ -72,3 +74,79 @@ class CobraWrapperTest(TestCase):
             ]
         )
         self.assertEqual(str(test_cobra_model.objective.direction), 'max')
+
+    def test_cobra_url_post(self):
+        """Still example on cobra doc to build a model, but using web API"""
+        metabolite_0_response = self.client.post('/cobra/metabolites/', dict(
+            base='ACP_c',
+            formula='C11H21N2O7PRS',
+            name='acyl-carrier-protein',
+            compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_0_response.status_code, 201)
+        metabolite_0_id = json.loads(metabolite_0_response.content)['id']
+        metabolite_1_response = self.client.post('/cobra/metabolites/', dict(
+            base='3omrsACP_c',
+            formula='C25H45N2O9PRS',
+            name='3-Oxotetradecanoyl-acyl-carrier-protein',
+            compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_1_response.status_code, 201)
+        metabolite_1_id = json.loads(metabolite_1_response.content)['id']
+        metabolite_2_response = self.client.post('/cobra/metabolites/', dict(
+            base='co2_c', formula='CO2', name='CO2', compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_2_response.status_code, 201)
+        metabolite_2_id = json.loads(metabolite_2_response.content)['id']
+        metabolite_3_response = self.client.post('/cobra/metabolites/', dict(
+            base='malACP_c',
+            formula='C14H22N2O10PRS',
+            name='Malonyl-acyl-carrier-protein',
+            compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_3_response.status_code, 201)
+        metabolite_3_id = json.loads(metabolite_3_response.content)['id']
+        metabolite_4_response = self.client.post('/cobra/metabolites/', dict(
+            base='h_c', formula='H', name='H', compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_4_response.status_code, 201)
+        metabolite_4_id = json.loads(metabolite_4_response.content)['id']
+        metabolite_5_response = self.client.post('/cobra/metabolites/', dict(
+            base='ddcaACP_c',
+            formula='C23H43N2O8PRS',
+            name='Dodecanoyl-ACP-n-C120ACP',
+            compartment='c'
+        ), content_type='application/json')
+        self.assertEqual(metabolite_5_response.status_code, 201)
+        metabolite_5_id = json.loads(metabolite_5_response.content)['id']
+        reaction_response = self.client.post('/cobra/reactions/', dict(
+            base='3OAS140',
+            name='3 oxoacyl acyl carrier protein synthase n C140 ',
+            subsystem='Cell Envelope Biosynthesis',
+            lower_bound=0,
+            upper_bound=1000,
+            metabolites=[
+                metabolite_0_id, metabolite_1_id, metabolite_2_id,
+                metabolite_3_id, metabolite_4_id, metabolite_5_id
+            ],
+            coefficients='-1.0 -1.0 -1.0 1.0 1.0 1.0',
+            gene_reaction_rule='( STM2378 or STM1197 )'
+        ), content_type='application/json')
+        self.assertEqual(reaction_response.status_code, 201)
+        reaction_id = json.loads(reaction_response.content)['id']
+        model_response = self.client.post('/cobra/models/', dict(
+            base='example_model',
+            objective='3OAS140',
+            reactions=[reaction_id]
+        ), content_type='application/json')
+        self.assertEqual(model_response.status_code, 201)
+        model_id = json.loads(model_response.content)['id']
+        model = cobra_models.CobraModel.objects.get(id=model_id)
+        cobra_model = model.build()
+        self.assertTrue(
+            str(cobra_model.objective.expression) in [
+                '-1.0*3OAS140_reverse_65ddc + 1.0*3OAS140',
+                '1.0*3OAS140 - 1.0*3OAS140_reverse_65ddc'
+            ]
+        )
+        self.assertEqual(str(cobra_model.objective.direction), 'max')
