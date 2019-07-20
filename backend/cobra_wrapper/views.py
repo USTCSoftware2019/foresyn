6ab4044687e1cd50a43bc4682d0c8e91f1ca35bc
myl7 <myl.ustc@gmail.com>
2019-07-20 21:05:06 +0800

    Change format only to fit 119 colume limit

--- backend/cobra_wrapper/views.py
+++ backend/cobra_wrapper/views.py
@@ -15,29 +15,15 @@ class CobraModelApi(View):
     def post(self, request):
         params = json.loads(request.body)
         try:
-            reactions = [
-                CobraReaction.objects.get(id=reaction_id)
-                for reaction_id in params['reactions']
-            ]
-            model = CobraModel(**get_required_params(params, [
-                'identifier', 'objective'
-            ]))
+            reactions = [CobraReaction.objects.get(id=reaction_id) for reaction_id in params['reactions']]
+            model = CobraModel(**get_required_params(params, ['identifier', 'objective']))
             model.full_clean()
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
         except AttributeError as error:
-            return JsonResponse({
-                'code': 200011,
-                'message': error.args
-            }, status=400)
+            return JsonResponse({'code': 200011, 'message': error.args}, status=400)
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200001,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200001, 'message': error.messages}, status=400)
         model.save()
         model.reactions.set(reactions)
         model.save()
@@ -53,38 +39,24 @@ class CobraModelApi(View):
             dmodel_base.delete()
             return JsonResponse({'code': 200, 'message': 'success'})
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
 
     def patch(self, request):
         params = json.loads(request.body)
         try:
             model = CobraModel.objects.get(id=params['id'])
             if 'reactions' in params.keys():
-                reactions = [
-                    CobraReaction.objects.get(id=reaction_id)
-                    for reaction_id in params['reactions']
-                ]
+                reactions = [CobraReaction.objects.get(id=reaction_id) for reaction_id in params['reactions']]
                 model.reactions.set(reactions)
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
-        for field in [
-            'identifier', 'objective'
-        ]:
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
+        for field in ['identifier', 'objective']:
             if field in params.keys():
                 setattr(model, field, params[field])
         try:
             model.full_clean()
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200001,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200001, 'message': error.messages}, status=400)
         model.save()
         return JsonResponse({'id': model.id}, status=200)
 
@@ -93,32 +65,17 @@ class CobraReactionApi(View):
     def post(self, request):
         params = json.loads(request.body)
         try:
-            metabolites = [
-                CobraMetabolite.objects.get(id=metabolite_id)
-                for metabolite_id in params['metabolites']
-            ]
+            metabolites = [CobraMetabolite.objects.get(id=metabolite_id) for metabolite_id in params['metabolites']]
             reaction = CobraReaction(**get_required_params(params, [
-                'identifier', 'name', 'subsystem', 'lower_bound',
-                'upper_bound', 'gene_reaction_rule'
-            ]))
-            reaction.coefficients = ' '.join(
-                map(lambda num: str(num), params['coefficients']))
+                'identifier', 'name', 'subsystem', 'lower_bound', 'upper_bound', 'gene_reaction_rule']))
+            reaction.coefficients = ' '.join(map(lambda num: str(num), params['coefficients']))
             reaction.full_clean()
         except AttributeError as error:
-            return JsonResponse({
-                'code': 200022,
-                'message': error.args
-            }, status=400)
+            return JsonResponse({'code': 200022, 'message': error.args}, status=400)
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200012,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200012, 'message': error.messages}, status=400)
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200002,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200002, 'message': error.messages}, status=400)
         reaction.save()
         reaction.metabolites.set(metabolites)
         reaction.save()
@@ -134,10 +91,7 @@ class CobraReactionApi(View):
             dreaction.delete()
             return JsonResponse({'code': 200, 'status': 'success'})
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
 
     def patch(self, request):
         params = json.loads(request.body)
@@ -145,31 +99,19 @@ class CobraReactionApi(View):
             reaction = CobraReaction.objects.get(id=params['id'])
             if 'metabolites' in params.keys():
                 metabolites = [
-                    CobraMetabolite.objects.get(id=metabolite_id)
-                    for metabolite_id in params['metabolites']
-                ]
+                    CobraMetabolite.objects.get(id=metabolite_id) for metabolite_id in params['metabolites']]
                 reaction.metabolites.set(metabolites)
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
-        for field in [
-            'identifier', 'name', 'subsystem', 'lower_bound', 'upper_bound',
-            'gene_reaction_rule'
-        ]:
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
+        for field in ['identifier', 'name', 'subsystem', 'lower_bound', 'upper_bound', 'gene_reaction_rule']:
             if field in params.keys():
                 setattr(reaction, field, params[field])
         if 'coefficients' in params.keys():
-            reaction.coefficients = ' '.join(
-                map(lambda num: str(num), params['coefficients']))
+            reaction.coefficients = ' '.join(map(lambda num: str(num), params['coefficients']))
         try:
             reaction.full_clean()
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200001,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200001, 'message': error.messages}, status=400)
         reaction.save()
         return JsonResponse({'id': reaction.id}, status=200)
 
@@ -179,19 +121,12 @@ class CobraMetaboliteApi(View):
         params = json.loads(request.body)
         try:
             metabolite = CobraMetabolite(**get_required_params(params, [
-                'identifier', 'formula', 'name', 'compartment'
-            ]))
+                'identifier', 'formula', 'name', 'compartment']))
             metabolite.full_clean()
         except AttributeError as error:
-            return JsonResponse({
-                'code': 200013,
-                'message': error.args
-            }, status=400)
+            return JsonResponse({'code': 200013, 'message': error.args}, status=400)
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200003,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200003, 'message': error.messages}, status=400)
         metabolite.save()
         return JsonResponse({'id': metabolite.id}, status=201)
 
@@ -205,32 +140,21 @@ class CobraMetaboliteApi(View):
             dmetabolite.delete()
             return JsonResponse({'status': 'success'})
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
 
     def patch(self, request):
         params = json.loads(request.body)
         try:
             metabolite = CobraMetabolite.objects.get(id=params['id'])
         except ObjectDoesNotExist as error:
-            return JsonResponse({
-                'code': 200021,
-                'message': error.messages
-            }, status=400)
-        for field in [
-            'identifier', 'formula', 'name', 'compartment'
-        ]:
+            return JsonResponse({'code': 200021, 'message': error.messages}, status=400)
+        for field in ['identifier', 'formula', 'name', 'compartment']:
             if field in params.keys():
                 setattr(metabolite, field, params[field])
         try:
             metabolite.full_clean()
         except ValidationError as error:
-            return JsonResponse({
-                'code': 200001,
-                'message': error.messages
-            }, status=400)
+            return JsonResponse({'code': 200001, 'message': error.messages}, status=400)
         metabolite.save()
         return JsonResponse({'id': metabolite.id}, status=200)
 
