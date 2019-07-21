c6912587294ab69d8a3ec90aa7331798ec0c79d4
LiuShige123 <liushige@mail.ustc.edu.cn>
2019-07-21 15:58:54 +0800

    add search by name

--- bigg_database/bigg_database/views.py
+++ bigg_database/bigg_database/views.py
@@ -12,7 +12,7 @@ class GetModelById(View):
         model_instance = Model.objects.get(bigg_id=bigg_id)
 
         mod_ins = {
-            "bigg_id":bigg_id,
+            "bigg_id":model_instance.bigg_id,
             "compartments":model_instance.compartments,
             "version",model_instance.version,
         }
@@ -32,7 +32,7 @@ class GetReactionById(View):
         reaction_instance = Reaction.objects.get(bigg_id=bigg_id)
 
         reac_ins = {
-            "bigg_id":bigg_id,
+            "bigg_id":reaction_instance.bigg_id,
             "name":reaction_instance.name,
             "reaction_string":reaction_instance.reaction_string,
             "pseudoreaction":reaction_instance.pseudoreaction,
@@ -53,7 +53,70 @@ class GetMetaboliteById(View):
         metabolite_instance = Metabolite.objects.get(bigg_id=bigg_id)
 
         meta_ins = {
+            "bigg_id":metabolite_instance.bigg_id,
+            "name":metabolite_instance.name,
+            "formulae":metabolite_instance.formulae,
+            "charges":metabolite_instance.charges,
+            "database_links":metabolite_instance.database_links,
+        }
+        return JsonResponse(json.dumps(meta_ins))
+
+    def get(self,request):
+        pass
+
+
+"""class GetModelByName(View):
+    def post(self,request):
+        try:
+            name = request.POST.get('name')
+        except ValueError:
+            pass
+        
+        model_instance = Model.objects.get(name=name)
+
+        mod_ins = {
             "bigg_id":bigg_id,
+            "compartments":model_instance.compartments,
+            "version",model_instance.version,
+        }
+
+        return JsonResponse(json.dumps(mod_ins))
+    
+    def get(self,request):
+        pass""" # model不含name
+
+class GetReactionByName(View):
+    def post(self,request):
+        try:
+            name = request.POST.get('name')
+        except ValueError:
+            pass
+
+        reaction_instance = Reaction.objects.get(name=name)
+
+        reac_ins = {
+            "bigg_id":reaction_instance.bigg_id,
+            "name":reaction_instance.name,
+            "reaction_string":reaction_instance.reaction_string,
+            "pseudoreaction":reaction_instance.pseudoreaction,
+            "database_links":reaction_instance.database_links,
+        }
+        return JsonResponse(json.dumps(reac_ins))
+
+    def get(self,request):
+        pass
+
+class GetMetaboliteByName(View):
+    def post(self,reqeust):
+        try:
+            name = request.POST.get('name')
+        except ValueError:
+            pass
+
+        metabolite_instance = Metabolite.objects.get(name=name)
+
+        meta_ins = {
+            "bigg_id":metabolite_instance.bigg_id,
             "name":metabolite_instance.name,
             "formulae":metabolite_instance.formulae,
             "charges":metabolite_instance.charges,
