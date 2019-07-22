06472a08eed37fa191d69ce86e3914c962307784
Elsa <zeyugao@outlook.com>
2019-07-22 19:08:36 +0800

    Update: Use smaller data set to test Use reverse instead of bare url

--- bigg_database/bigg_database/tests.py
+++ bigg_database/bigg_database/tests.py
@@ -1,6 +1,19 @@
 import json
-from django.test import TestCase, Client
-from .models import Model, Reaction, Metabolite
+
+from django.shortcuts import reverse
+from django.test import Client, TestCase
+
+from .models import Metabolite, Model, Reaction
+
+from urllib.parse import urlencode
+
+
+def build_url(*args, **kwargs):
+    get = kwargs.pop('get', {})
+    url = reverse(*args, **kwargs)
+    if get:
+        url += '?' + urlencode(get)
+    return url
 
 
 class IdSearchTests(TestCase):
@@ -12,76 +25,67 @@ class IdSearchTests(TestCase):
         data = {
             "bigg_id": "iAF692"
         }
-        resp = client.post('search/model/id', data)
-        print(resp.content)
+        resp = client.get(build_url('search_model', get=data))
         mod_ins = json.loads(resp.content)['content']
 
-        self.assertEqual(mod_ins['compartments'], {
-            "c": "cytosol",
-            "e": "extracellular space"
-        })
+        self.assertEqual(mod_ins['compartments'], ["c", "e"])
         self.assertEqual(mod_ins['version'], "1")
 
         data = {
             "bigg_id": "iAPECO1_1312",
         }
-        resp = client.post('search/model/id', data)
+        resp = client.get(build_url('search_model', get=data))
         mod_ins = json.loads(resp.content)
 
-        self.assertEqual(mod_ins['compartments'], {
-            "c": "cytosol",
-            "e": "extracellular space",
-            "p": "periplasm"
-        })
+        self.assertEqual(mod_ins['compartments'], {"c", "e", "p"})
         self.assertEqual(mod_ins['version'], "1")
 
     def test_id_reaction(self):
 
         client = Client()
         data = {
-            "bigg_id":"1a25DHVITD3TRn"
+            "bigg_id": "PLDAGAT_MYRS_EPA_MYRS_PC_3_c"
         }
-        resp = client.post('search/reaction/id', data)
+        resp = client.get(build_url('search_reaction', get=data))
         reac_ins = json.loads(resp.content)
 
         self.assertEqual(reac_ins['name'],
-            "Tanslocation of 1-alpha,25-Dihydroxyvitamin D3 to nucleus")
+                         "Phospholipid: diacylglycerol acyltransferase (14:0/20:5(5Z,8Z,11Z,14Z,17Z)/14:0)")
         self.assertEqual(reac_ins['reaction_string'],
-            "1a25dhvitd3_c &#8652; 1a25dhvitd3_n")
+                         "12dgr140205n3_c + pc1619Z140_c &#8652; 1agpc161_c + tag140205n3140_c")
         self.assertEqual(reac_ins['pseudoreaction'], False)
 
-        data = {"bigg_id": "1AGPEAT1819Z1835Z9Z12Z"}
-        resp = client.post('search/reaction/id', data)
+        data = {"bigg_id": "ACACT1m"}
+        resp = client.get(build_url('search_reaction', get=data))
         reac_ins = json.loads(resp.content)
 
         self.assertEqual(reac_ins['name'],
-            "1-acylglycerophosphoethanolamine O-acyltransferase (18:1(9Z)/18:3(5Z,12Z,15Z))")
+                         "Acetyl CoA C acetyltransferase  mitochondrial")
         self.assertEqual(reac_ins['reaction_string'],
-            "1agpe1819Z_c + pacoa_c &#8652; coa_c + pe1819Z1835Z9Z12Z_c")
+                         "2.0 accoa_m &#8652; aacoa_m + coa_m")
         self.assertEqual(reac_ins['pseudoreaction'], False)
 
     def test_id_metabolite(self):
 
         client = Client()
-        data = {"bigg_id": "1a2425thvitd3"}
-        resp = client.post('search/metabolite/id',data) 
+        data = {"bigg_id": "nac_e"}
+        resp = client.get(build_url('search_metabolite', get=data))
         meta_ins = json.loads(resp.content)
 
         self.assertEqual(meta_ins['name'],
-            "1-alpha,24R,25-Trihydroxyvitamin D3")
-        self.assertEqual(meta_ins['formulae'], ["C27H44O4"])
-        self.assertEqual(meta_ins['charges'], [0])
+                         "Nicotinate")
+        self.assertEqual(meta_ins['formulae'], ["C6H4NO2"])
+        self.assertEqual(meta_ins['charges'], -1)
 
-
-        data = {"bigg_id":"1agpc_SC"}
-        resp = client.post('search/metabolite/id',data) 
+        data = {"bigg_id": "prephthcoa_c"}
+        resp = client.get(build_url('search_metabolite', get=data))
         meta_ins = json.loads(resp.content)
 
         self.assertEqual(meta_ins['name'],
-            "1 Acyl sn glycerol 3 phosphocholine  yeast specific C2420H4922N100O700P100")
+                         "Phthiocerol precursor bound coenzyme A")
         self.assertEqual(meta_ins['formulae'],
-            ["C2420H4922N100O700P100"])
-        self.assertEqual(meta_ins['charges'], [0])
+                         ["C53H92N7O20P3S"])
+        self.assertEqual(meta_ins['charges'], 0)
 
 
 class NameSearchTests(TestCase):
@@ -90,22 +94,23 @@ class NameSearchTests(TestCase):
 
         client = Client()
         data = {"name": "1-alpha,24R,25-Vitamin D-hydroxylase (D2)"}
-        resp = client.post('search/reaction/name',data) 
+        resp = client.get(build_url('search_reaction', get=data))
         reac_ins = json.loads(resp.content)
 
         self.assertEqual(reac_ins['bigg_id'], "1a_25VITD2Hm")
         self.assertEqual(reac_ins['reaction_string'],
-        "h_m + nadph_m + o2_m + 1a25dhvitd2_m &#8652; h2o_m + nadp_m + 1a2425thvitd2_m")
+                         "h_m + nadph_m + o2_m + 1a25dhvitd2_m &#8652; h2o_m + nadp_m + 1a2425thvitd2_m")
         self.assertEqual(reac_ins['pseudoreaction'], False)
 
         data = {
-            "name":"1-Aminocyclopropane-1-carboxylate transport, mitochondria"
+            "name": "1-Aminocyclopropane-1-carboxylate transport, mitochondria"
         }
-        resp = client.post('search/reaction/name',data) 
+        resp = client.get(build_url('search_reaction', get=data))
         reac_ins = json.loads(resp.content)
 
         self.assertEqual(reac_ins['bigg_id'], "1ACPCtm")
-        self.assertEqual(reac_ins['reaction_string'], "1acpc_c &#8652; 1acpc_m")
+        self.assertEqual(reac_ins['reaction_string'],
+                         "1acpc_c &#8652; 1acpc_m")
         self.assertEqual(reac_ins['pseudoreaction'], False)
 
     def test_name_metabolite(self):
@@ -114,20 +119,17 @@ class NameSearchTests(TestCase):
         data = {
             "name": "Sulfate"
         }
-        resp = client.post('search/metabolite/name',data) 
+        resp = client.get(build_url('search_metabolite', get=data))
         meta_ins = json.loads(resp.content)
 
         self.assertEqual(meta_ins['bigg_id'], "so4")
-        self.assertEqual(meta_ins['formulae'], [
-            "SO4",
-            "O4S"
-        ])
+        self.assertEqual(meta_ins['formulae'], ["SO4", "O4S"])
         self.assertEqual(meta_ins['charges'], -2)
 
         data = {"name": "1-14:0-2-lysophosphatidylcholine"}
-        resp = client.post('search/metabolite/name',data) 
+        resp = client.post('search/metabolite/name', data)
         meta_ins = json.loads(resp.content)
 
         self.assertEqual(meta_ins['bigg_id'], "1agpc140")
         self.assertEqual(meta_ins['formulae'], ["C22H46NO7P"])
-        self.assertEqual(meta_ins['charges'], [0])
+        self.assertEqual(meta_ins['charges'], 0)
