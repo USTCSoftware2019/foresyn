78233f05fa0d3be1ef49752b60e9cf3d80261e4f
Elsa <zeyugao@outlook.com>
2019-07-22 13:02:00 +0800

    Fix process gene, avoiding null Fix import error

--- bigg_database/bigg_database/management/commands/process_gene.py
+++ bigg_database/bigg_database/management/commands/process_gene.py
@@ -4,14 +4,28 @@ import os
 
 from django.core.management.base import BaseCommand
 
-from bigg_database.models import Metabolite
+from bigg_database.models import Gene
 
 
 def get_from_content(content, *argv):
     return {
-        key: getattr(content, key)
+        key: content[key]
         for key in argv
     }
+
+
+type_default = {
+    'int': 0,
+    'str': '',
+    'json': r'{}',
+    'bool': True,
+}
+
+
+def avoid_null(stuff, val_type, *argv):
+    for key in argv:
+        stuff[key] = stuff[key] or type_default[val_type]
+    return stuff
 
 
 def main(gene_path):
@@ -31,7 +45,12 @@ def main(gene_path):
                                          'mapped_to_genbank', 'leftpos', 'database_links',
                                          'strand', 'protein_sequence', 'genome_name',
                                          'dna_sequence', 'bigg_id', 'genome_ref_string')
-            except KeyError as e:
+                stuff = avoid_null(stuff, 'int', 'rightpos', 'leftpos')
+                stuff = avoid_null(stuff, 'bool', 'mapped_to_genbank')
+                stuff = avoid_null(stuff, 'str', 'name', 'dna_sequence',
+                                   'genome_ref_string', 'protein_sequence', 'genome_name',
+                                   'chromosome_ncbi_accession', 'strand')
+            except AttributeError as e:
                 print(e, file)
                 continue
 
