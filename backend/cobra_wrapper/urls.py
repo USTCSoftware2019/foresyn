4969bbbadcaa30a6679de1e43ee316c0bf95088d
myl7 <myl.ustc@gmail.com>
2019-07-27 20:24:19 +0800

    cobra_wrapper: unrest

--- backend/cobra_wrapper/urls.py
+++ backend/cobra_wrapper/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path
 
-from . import views
+from . import api, views
+
+ENABLE_API = False
 
 app_name = 'cobra'
 urlpatterns = [
-    path('metabolites/', views.CobraMetaboliteSetApi.as_view(), name='metabolite_set'),
-    path('metabolites/<int:model_id>/', views.CobraMetaboliteObjectApi.as_view(), name='metabolite_object'),
-    path('reactions/', views.CobraReactionSetApi.as_view(), name='reaction_set'),
-    path('reactions/<int:model_id>/', views.CobraReactionObjectApi.as_view(), name='reaction_object'),
-    path('models/', views.CobraModelSetApi.as_view(), name='model_set'),
-    path('models/<int:model_id>/', views.CobraModelObjectApi.as_view(), name='model_object'),
-    path(
-        'models/<int:model_id>/<str:method>/',
-        views.CobraModelObjectComputeApi.as_view(),
-        name='model_object_computation'
-    ),
+    *([
+        path('api/metabolites/', api.CobraMetaboliteSetApi.as_view(), name='metabolite_set'),
+        path('api/metabolites/<int:model_id>/', api.CobraMetaboliteObjectApi.as_view(), name='metabolite_object'),
+        path('api/reactions/', api.CobraReactionSetApi.as_view(), name='reaction_set'),
+        path('api/reactions/<int:model_id>/', api.CobraReactionObjectApi.as_view(), name='reaction_object'),
+        path('api/models/', api.CobraModelSetApi.as_view(), name='model_set'),
+        path('api/models/<int:model_id>/', api.CobraModelObjectApi.as_view(), name='model_object'),
+        path(
+            'api/models/<int:model_id>/<str:method>/',
+            api.CobraModelObjectComputeApi.as_view(),
+            name='model_object_computation'
+        )
+    ] if ENABLE_API else []),
 ]
