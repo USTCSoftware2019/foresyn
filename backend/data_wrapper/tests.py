53c617c11e7bcff394ba7701f6673149fda0e70e
biuren <biuren@mail.ustc.edu.cn>
2019-07-27 18:08:11 +0800

    Modified methods and added tests

--- backend/data_wrapper/tests.py
+++ backend/data_wrapper/tests.py
@@ -1,15 +1,38 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 
 class ModelTest(TestCase):
-    fixtures = ['bigg_database/test_data']
-    client = Client()
+    fixtures = ['bigg_database/test_data',
+                'bigg_database/test_gene_data',
+                'bigg_database/test_relationship_data'
+                ]
+
+    def _create_user_and_login(self):
+        user_info = {'username': 'test', 'password': '12345678'}
+        user = User.objects.create_user(**user_info)
+        self.client = Client()
+        self.client.login(**user_info)
+        return user
 
     def test_add_model(self):
         # TBD
         pass
 
+    # def test_add_reactions(self):
+    #     self._create_user_and_login()
+    #     pk = 1
+    #     res = self.client.post("/data_wrapper/add_reactions/", {"pk": pk}).status_code
+    #     self.assertEqual(res, 200)
+
     def test_add_metabolite(self):
+        self._create_user_and_login()
         pk = 1
-        res = self.client.post("/data_wrapper/add_metabolites", {"pk": pk})
-        self.assertEqual(res, 'test')
+        res = self.client.post("/data_wrapper/add_metabolites/", {"pk": pk}).status_code
+        self.assertEqual(res, 200)
+
+    # def test_add_metabolite_without_login(self):
+    #     self.client = Client()
+    #     pk = 1
+    #     res = self.client.post("/data_wrapper/add_metabolites/", {"pk": pk}).status_code
+    #     self.assertEqual(res, 401)
--- backend/data_wrapper/views.py
+++ backend/data_wrapper/views.py
@@ -1,5 +1,4 @@
 from django.views import View
-
 from cobra_wrapper.models import CobraModel
 from bigg_database.models import Model as DataModel
 from django.http import JsonResponse
@@ -8,7 +7,9 @@ from .common import data_metabolite_to_cobra_metabolite, data_reaction_to_cobra_
 
 class AddDataModelToCobra(View):
     def post(self, request, database_pk):
-        # user = request.user
+        if not request.user.is_authenticated:
+            return JsonResponse({"status_code": 401})
+        user = request.user
         data_model_object = DataModel.objects.get(pk=database_pk)
         cobra_model_object = CobraModel()
         # relationship
@@ -16,36 +17,44 @@ class AddDataModelToCobra(View):
         cobra_model_object.name = data_model_object.bigg_id
 
         # add reactions first
-
         for data_modelreaction in data_model_object.modelreaction_set.all():
             for data_reaction_object in data_modelreaction.reaction_set.all():
                 cobra_reaction_object = data_reaction_to_cobra_reaction(
-                    # user=user,
+                    user=user,
                     data_reaction_object=data_reaction_object,
                     subsystem=data_modelreaction.subsystem,
                     upper_bound=data_modelreaction.upper_bound,
                     lower_bound=data_modelreaction.lower_bound
                 )
                 cobra_reaction_object.save()
+                cobra_model_object.reactions.add(cobra_reaction_object)
 
-        # cobra_model_object.user = user
+        cobra_model_object.owner = user
         cobra_model_object.save()
-        return JsonResponse("test")
+        return JsonResponse({"status_code": 200})
 
 
 class AddDataReactionToCobra(View):
     def post(self, request):
-        # user = request.user
+        if not request.user.is_authenticated:
+            return JsonResponse({"status_code": 401})
+        user = request.user
         database_pk = request.POST["pk"]
-        cobra_reaction_object = data_reaction_to_cobra_reaction(key="pk", value=database_pk)
+        cobra_reaction_object = data_reaction_to_cobra_reaction(key="pk", value=database_pk, user=user)
+        if cobra_reaction_object is None:
+            return JsonResponse({"status_code": 500})
         cobra_reaction_object.save()
-        return JsonResponse('test')
+        return JsonResponse({"status_code": 200})
 
 
 class AddDataMetaboliteToCobra(View):
     def post(self, request):
-        # user = request.user
+        if not request.user.is_authenticated:
+            return JsonResponse({"status_code": 401})
+        user = request.user
         database_pk = request.POST["pk"]
-        cobra_metabolite_object = data_metabolite_to_cobra_metabolite(key="pk", value=database_pk)
+        cobra_metabolite_object = data_metabolite_to_cobra_metabolite(key="pk", value=database_pk, user=user)
+        if cobra_metabolite_object is None:
+            return JsonResponse({"status_code": 500})
         cobra_metabolite_object.save()
-        return JsonResponse("test")
+        return JsonResponse({"status_code": 200})
