f1b40c8043f49676d4b1f2c20a831445347e2c59
taoky <taoky1999@gmail.com>
2019-07-27 22:08:42 +0800

    writing signup part

---
+++ backend/accounts/tokens.py
@@ -0,0 +1,14 @@
+# https://stackoverflow.com/questions/50298114/django-2-how-to-register-a-user-using-email-confirmation-and-cbvs
+
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_active)
+        )
+
+
+account_activation_token = TokenGenerator()
