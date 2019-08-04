7a13c077dc54dce559435d9c8c6d1035c7cb1622
lbc12345 <1668188175@qq.com>
2019-08-05 00:14:42 +0800

    cobra_wrapper: Complete tests, fix a bug in metabolite_delete template

--- backend/cobra_wrapper/tests.py
+++ backend/cobra_wrapper/tests.py
@@ -23,6 +23,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='C11H21N2O7PRS',
                 name='acyl-carrier-protein',
                 compartment='c',
+                charge='1',
                 owner=user
             ),
             CobraMetabolite.objects.create(
@@ -30,6 +31,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='C25H45N2O9PRS',
                 name='3-Oxotetradecanoyl-acyl-carrier-protein',
                 compartment='c',
+                charge='1',
                 owner=user
             ),
             CobraMetabolite.objects.create(
@@ -37,6 +39,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='CO2',
                 name='CO2',
                 compartment='c',
+                charge='1',
                 owner=user
             ),
             CobraMetabolite.objects.create(
@@ -44,6 +47,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='C14H22N2O10PRS',
                 name='Malonyl-acyl-carrier-protein',
                 compartment='c',
+                charge='1',
                 owner=user
             ),
             CobraMetabolite.objects.create(
@@ -51,6 +55,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='H',
                 name='H',
                 compartment='c',
+                charge='1',
                 owner=user
             ),
             CobraMetabolite.objects.create(
@@ -58,6 +63,7 @@ class CobraWrapperViewTests(TestCase):
                 formula='C23H43N2O8PRS',
                 name='Dodecanoyl-ACP-n-C120ACP',
                 compartment='c',
+                charge='1',
                 owner=user
             )
         ]
@@ -68,6 +74,7 @@ class CobraWrapperViewTests(TestCase):
             subsystem='Cell Envelope Biosynthesis',
             lower_bound=0,
             upper_bound=1000,
+            objective_coefficient=0,
             coefficients='-1.0 -1.0 -1.0 1.0 1.0 1.0',
             gene_reaction_rule='( STM2378 or STM1197 )',
             owner=user
@@ -76,6 +83,7 @@ class CobraWrapperViewTests(TestCase):
 
         model = CobraModel.objects.create(
             cobra_id='example_model',
+            name='test',
             objective='3OAS140',
             owner=user
         )
@@ -173,7 +181,7 @@ class CobraWrapperViewTests(TestCase):
         response = self.client.get('/cobra/reactions/7777777/')
         self.assertEqual(response.status_code, 404)
 
-        # metabolite_0_response = self.client.post('/cobra/metabolites/', dict(  # FIXME:
+        # metabolite_0_response = self.client.post('/cobra/metabolites/', dict(
         #     cobra_id='ACP_c',
         #     formula='C11H21N2O7PRS',
         #     name='acyl-carrier-protein',
@@ -279,9 +287,9 @@ class CobraWrapperViewTests(TestCase):
         self.assertTemplateNotUsed('cobra_wrapper/cobrametabolite_detail.html')
 
         response = self.client.get('/cobra/metabolites/create/')
-        self.assertContains(response, '<input type="reset" value="Reset">')
-        self.assertContains(response, '<input type="submit" value="Create">')
-        self.assertContains(response, '<a href="/cobra/metabolites/">Return</a>')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Create">', html=True)
+        self.assertContains(response, '<a href="/cobra/metabolites/">Return</a>', html=True)
         for comp in ['cobra_id', 'name', 'formula', 'charge', 'compartment']:
             self.assertContains(response, comp)
 
@@ -305,7 +313,6 @@ class CobraWrapperViewTests(TestCase):
             lower_bound=0,
             upper_bound=1000,
             objective_coefficients=0,
-            metabolites=[1, 2, 3, 4, 5, 6],  # TODO: how to show metabolites as manytomany field
             coefficients='-1.0 -1.0 -1.0 1.0 1.0 1.0',
             gene_reaction_rule='test'))
         self.assertEqual(response.status_code, 200)  # FIXME: why 200 not 302?
@@ -314,30 +321,208 @@ class CobraWrapperViewTests(TestCase):
         self.assertTemplateNotUsed('cobra_wrapper/cobrareaction_detail.html')
 
         response = self.client.get('/cobra/reactions/create/')
-        self.assertContains(response, '<input type="reset" value="Reset">')
-        self.assertContains(response, '<input type="submit" value="Create">')
-        self.assertContains(response, '<a href="/cobra/reactions/">Return</a>')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Create">', html=True)
+        self.assertContains(response, '<a href="/cobra/reactions/">Return</a>', html=True)
         for comp in ['cobra_id', 'name', 'subsystem', 'lower_bound', 'upper_bound', 'objective_coefficient',
                      'metabolites', 'coefficients', 'gene_reaction_rule']:
             self.assertContains(response, comp)
 
-    def test_create_fail(self):
+    def test_create_models(self):
         self._create_user_and_login()
+        self.client.post('/cobra/models/create/', dict(
+            cobra_id='test',
+            name='test',
+            objective='test' * 50))
+        self.assertRaises(ValidationError)
 
-    def test_update_ok(self):
-        info = self._create_models(self._create_user_and_login())
+        response = self.client.post('/cobra/models/create/', dict(
+            cobra_id='test',
+            name='test',
+            objective='test'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_create_form.html')
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_list.html')
+        self.assertTemplateNotUsed('cobra_wrapper/cobramodel_detail.html')
 
-    def test_update_fail(self):
-        info = self._create_models(self._create_user_and_login())
+        response = self.client.get('/cobra/models/create/')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Create">', html=True)
+        self.assertContains(response, '<a href="/cobra/models/">Return</a>', html=True)
+        for comp in ['cobra_id', 'name', 'reactions', 'objective']:
+            self.assertContains(response, comp)
 
-    def test_delete_ok(self):
-        info = self._create_models(self._create_user_and_login())
+    def test_update_metabolites(self):
+        self._create_models(self._create_user_and_login())
+        response = self.client.post('/cobra/metabolites/7777777/update/', dict(
+            cobra_id='test'
+        ))
+        self.assertEqual(response.status_code, 404)
 
-    def test_delete_fail(self):
-        info = self._create_models(self._create_user_and_login())
+        self.client.post('/cobra/metabolites/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            formula='test',
+            charge='test',
+            compartment='test' * 50))
+        self.assertRaises(ValidationError)
+
+        response = self.client.post('/cobra/metabolites/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            formula='test',
+            charge='test',
+            compartment='test'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('cobra_wrapper/cobrametabolite_update_form.html')
+        self.assertTemplateUsed('cobra_wrapper/cobrametabolite_detail.html')
+        self.assertTemplateNotUsed('cobra_wrapper/cobrametabolite_create_form.html')
+
+        response = self.client.get('/cobra/metabolites/1/update/')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Update">', html=True)
+        self.assertContains(response, '<a href="/cobra/metabolites/1/">Return</a>', html=True)
+        for comp in ['cobra_id', 'name', 'formula', 'charge', 'compartment']:
+            self.assertContains(response, comp)
+
+    def test_update_reactions(self):
+        self._create_models(self._create_user_and_login())
+        response = self.client.post('/cobra/models/7777777/update/', dict(
+            cobra_id='test'
+        ))
+        self.assertEqual(response.status_code, 404)
+
+        self.client.post('/cobra/models/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            subsystem='test',
+            lower_bound=0,
+            upper_bound=1000,
+            objective_coefficients=0,
+            coefficients='-1.0, -1.0, -1.0, 1.0, 1.0, 1.0',
+            gene_reaction_rule='test'))
+        self.assertRaises(ValidationError)
+
+        response = self.client.post('/cobra/reactions/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            subsystem='test',
+            lower_bound=0,
+            upper_bound=1000,
+            objective_coefficients=0,
+            coefficients='-1.0 -1.0 -1.0 1.0 1.0 1.0',
+            gene_reaction_rule='test'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('cobra_wrapper/cobrareaction_update_form.html')
+        self.assertTemplateUsed('cobra_wrapper/cobrareaction_detail.html')
+        self.assertTemplateNotUsed('cobra_wrapper/cobrareaction_create_form.html')
+
+        response = self.client.get('/cobra/reactions/1/update/')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Update">', html=True)
+        self.assertContains(response, '<a href="/cobra/reactions/1/">Return</a>', html=True)
+        for comp in ['cobra_id', 'name', 'subsystem', 'lower_bound', 'upper_bound', 'objective_coefficient',
+                     'metabolites', 'coefficients', 'gene_reaction_rule']:
+            self.assertContains(response, comp)
+
+    def test_update_models(self):
+        self._create_models(self._create_user_and_login())
+        response = self.client.post('/cobra/metabolites/7777777/update/', dict(
+            cobra_id='test'
+        ))
+        self.assertEqual(response.status_code, 404)
+
+        self.client.post('/cobra/metabolites/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            objective='test' * 50))
+        self.assertRaises(ValidationError)
+
+        response = self.client.post('/cobra/metabolites/1/update/', dict(
+            cobra_id='test',
+            name='test',
+            objective='test'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_update_form.html')
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_detail.html')
+        self.assertTemplateNotUsed('cobra_wrapper/cobramodel_create_form.html')
+
+        response = self.client.get('/cobra/models/1/update/')
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Update">', html=True)
+        self.assertContains(response, '<a href="/cobra/models/1/">Return</a>', html=True)
+        for comp in ['cobra_id', 'name', 'reactions', 'objective']:
+            self.assertContains(response, comp)
+
+    def test_delete_metabolites(self):
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.post('/cobra/metabolites/7777777/delete/')
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get('/cobra/metabolites/1/delete/')
+        self.assertTemplateUsed('cobra_wrapper/cobrametabolite_confirm_delete.html')
+        self.assertContains(response, '<p>You can not delete the metabolite!</p>', html=True)
+
+        response = self.client.post('/cobra/metabolites/1/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_reactions(self):
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.post('/cobra/reactions/7777777/delete/')
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get('/cobra/reactions/1/delete/')
+        self.assertTemplateUsed('cobra_wrapper/cobrareaction_confirm_delete.html')
+        self.assertContains(response, '<p>You can not delete the reaction!</p>', html=True)
+
+        response = self.client.post('/cobra/reactions/1/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_models(self):
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.post('/cobra/models/7777777/delete/')
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get('/cobra/models/1/delete/')
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_confirm_delete.html')
+        self.assertContains(response, '<p>Are you sure to delete: example_model[test]?</p>', html=True)
+        self.assertContains(response, '<input type="submit" value="Confirm">', html=True)
+
+        response = self.client.post('/cobra/models/1/delete/')
+        self.assertEqual(response.status_code, 302)
 
     def test_fba(self):
-        info = self._create_models(self._create_user_and_login())
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.get('/cobra/models/1/fba/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_fba_detail.html')
+        for comp in ['objective_value', 'status', 'fluxes', 'shadow_price']:
+            self.assertContains(response, comp)
+        self.assertContains(response, '<a href="/cobra/models/1/">Return</a>', html=True)
+        self.assertNotContains(response, 'maximum')
+
+    def test_fva_create(self):
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.get('/cobra/models/1/fva/create/')
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_fva_create_form.html')
+        for comp in ['loopless', 'fraction_of_optimum', 'pfba_factor', 'reaction_list']:
+            self.assertContains(response, comp)
+        self.assertContains(response, '<input type="reset" value="Reset">', html=True)
+        self.assertContains(response, '<input type="submit" value="Submit">', html=True)
+        self.assertContains(response, '<a href="/cobra/models/1/">Return</a>', html=True)
+        self.assertNotContains(response, 'minimum')
 
     def test_fva(self):
-        info = self._create_models(self._create_user_and_login())
+        self._create_models(self._create_user_and_login())
+
+        response = self.client.get('/cobra/models/1/fva/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('cobra_wrapper/cobramodel_fva_detail.html')
+        for comp in ['name', 'maximum', 'minimum']:
+            self.assertContains(response, comp)
+        self.assertContains(response, '<a href="/cobra/models/1/">Return</a>', html=True)
