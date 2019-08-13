362249686de7a2dafcc933da424ee5c1946c1514
myl7 <myl.ustc@gmail.com>
2019-08-13 19:53:58 +0800

    cobra_wrapper: Change celeryconfig params to upper class

--- backend/cobra_wrapper/celeryconfig.py
+++ backend/cobra_wrapper/celeryconfig.py
@@ -1,8 +1,8 @@
-broker_url = 'amqp://test:test123456@localhost:5672/test'
-result_backend = 'rpc://'
-imports = ('cobra_wrapper.tasks',)
-timezone = 'Asia/Shanghai'
-task_annotations = {
+BROKER_URL = 'amqp://test:test123456@localhost:5672/test'
+RESULT_BACKEND = 'rpc://'
+IMPORTS = ('cobra_wrapper.tasks',)
+TIMEZONE = 'Asia/Shanghai'
+TASK_ANNOTATIONS = {
     'cobra_computation.tasks.cobra_fba': {
         'rate_limit': '10/s'
     },
