a3e87a99b348ba721280517226fccf6e09f8e671
myl7 <myl.ustc@gmail.com>
2019-08-15 03:05:09 +0800

    cobra_wrapper: Fix flake8

--- backend/cobra_wrapper/remote/cobra_computation/tasks.py
+++ backend/cobra_wrapper/remote/cobra_computation/tasks.py
@@ -20,7 +20,9 @@ def cobra_fba(cobra_model_json):
 @app.task
 def cobra_fva(cobra_model_json, reaction_list=None, loopless=False, fraction_of_optimum=1.0, pfba_factor=None):
     cobra_model = cobra.io.from_json(cobra_model_json)
-    result = flux_variability_analysis(cobra_model, reaction_list, loopless, fraction_of_optimum, pfba_factor).to_json()
+    result = flux_variability_analysis(
+        cobra_model, reaction_list, loopless, fraction_of_optimum, pfba_factor
+    ).to_json()
     app.send_task(
         'cobra_wrapper.tasks.cobra_fva_save',
         args=[result, app.current_task.request.id],
