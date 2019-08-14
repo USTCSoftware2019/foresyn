9538f612a6ed459537ae9ca420355d27e981362e
myl7 <myl.ustc@gmail.com>
2019-08-15 02:27:08 +0800

    cobra_wrapper: Add more buttoms and info to templates

--- backend/cobra_wrapper/templatetags/cobra_wrapper_tags.py
+++ backend/cobra_wrapper/templatetags/cobra_wrapper_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter
 def get_dict_value_with_key(dic, key):
     return dic[key]
+
+
+@register.filter
+def check_result_exist(fva_obj):
+    if fva_obj.result:
+        return 'OK'
+    else:
+        return 'Processing'
