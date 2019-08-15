e2675d942d2fefd0b688cc4de494de9339e1c789
myl7 <myl.ustc@gmail.com>
2019-08-15 12:45:12 +0800

    cobra_wrapper: Add signals to revoke celery tasks after deletion

--- backend/cobra_wrapper/apps.py
+++ backend/cobra_wrapper/apps.py
@@ -1,11 +1,13 @@
 from django.apps import AppConfig
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, pre_delete
 
 
 class CobraWrapperConfig(AppConfig):
     name = 'cobra_wrapper'
 
     def ready(self):
-        from .signals.celery_feeds import call_cobra_fba, call_cobra_fva
+        from .signals.celery_feeds import call_cobra_fba, call_cobra_fva, revoke_cobra_fba, revoke_cobra_fva
         pre_save.connect(call_cobra_fba, sender='cobra_wrapper.CobraFba', dispatch_uid='call_cobra_fba')
         pre_save.connect(call_cobra_fva, sender='cobra_wrapper.CobraFva', dispatch_uid='call_cobra_fva')
+        pre_delete.connect(revoke_cobra_fba, sender='cobra_wrapper.CobraFba', dispatch_uid='revoke_cobra_fba')
+        pre_delete.connect(revoke_cobra_fva, sender='cobra_wrapper.CobraFva', dispatch_uid='revoke_cobra_fva')
--- backend/cobra_wrapper/signals/celery_feeds.py
+++ backend/cobra_wrapper/signals/celery_feeds.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 import cobra
+from celery.result import AsyncResult
 
 from cobra_wrapper.models import CobraFba, CobraFva
 
@@ -40,3 +41,19 @@ def call_cobra_fva(sender, **kwargs):
         routing_key='cobra_feed.fva'
     )
     instance.task_id = result.id
+
+
+@receiver(pre_delete, sender=CobraFba)
+def revoke_cobra_fba(sender, **kwargs):
+    instance = kwargs['instance']
+    if not instance.result:
+        result = AsyncResult(instance.task_id, app=app)
+        result.revoke(terminate=True)
+
+
+@receiver(pre_delete, sender=CobraFva)
+def revoke_cobra_fva(sender, **kwargs):
+    instance = kwargs['instance']
+    if not instance.result:
+        result = AsyncResult(instance.task_id, app=app)
+        result.revoke(terminate=True)
