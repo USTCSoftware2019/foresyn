b91a8742e6d01ee45174030dfbb961abfbae6c54
Elsa <zeyugao@outlook.com>
2019-09-02 13:18:38 +0800

    template: Remove search_model selection Fix the passing param for search

--- backend/bigg_database/haystack_backend.py
+++ backend/bigg_database/haystack_backend.py
@@ -11,11 +11,12 @@ class FuzzyBackend(Elasticsearch2SearchBackend):
             # It does not support the AUTO in fuzziness
             # As for the elasticsearch 2.4.6, the version of Lucene is
             # much lower than 6.2.4
-            l = len(search_kwargs['query']['filtered']['query']['query_string']['query'])
-            l -= 3 # Remove '(', ')' and '~' in the query when calculating the length of the querying string
+            query_string_length = len(search_kwargs['query']['filtered']['query']['query_string']['query'])
+            # Remove '(', ')' and '~' in the query when calculating the length of the querying string
+            query_string_length -= 3
 
             # Refer: https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#fuzziness
-            fuzziness = 2 if l > 5 else (0 if l<=2 else 1)
+            fuzziness = 2 if query_string_length > 5 else (0 if query_string_length <= 2 else 1)
             search_kwargs['query']['filtered']['query']['query_string']['fuzziness'] = fuzziness
 
         except KeyError:
