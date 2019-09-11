93191e0f975e29663e759407a75817363ad20fdd
taoky <taoky1999@gmail.com>
2019-09-11 22:20:29 +0800

    disable elasticsearch temporarily

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/wait.sh -t 120 elasticsearch:9200 -- python manage.py rebuild_index --noinput
+#/wait.sh -t 120 elasticsearch:9200 -- python manage.py rebuild_index --noinput
 /wait.sh -t 120 mysql:3306 -- python manage.py runserver 0:8000
