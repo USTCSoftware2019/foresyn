43e8c309186c0375c671bab9338c18eff1e82b5a
biuren <biuren@mail.ustc.edu.cn>
2019-10-03 00:17:22 +0800

    biobricks: build indexes

--- backend/biobricks/models.py
+++ backend/biobricks/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 
-class Biobrick(models):
+class Biobrick(models.Model):
     part_name = models.CharField(unique=True, max_length=127)
-    description = models.TextField()
-    keywords = models.TextField()
-    uses = models.IntegerField()
+    description = models.TextField(null=True)
+    keywords = models.TextField(null=True)
+    uses = models.IntegerField(null=True)
+    url = models.URLField(null=True)
--- backend/biobricks/search_indexes.py
+++ backend/biobricks/search_indexes.py
@@ -5,8 +5,8 @@ from .models import Biobrick
 class BiobrickIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
-    description = indexes.CharField(model_attr="description")
-    keywords = indexes.CharField(model_attr="key_words")
+    description = indexes.CharField(model_attr="description", null=True)
+    keywords = indexes.CharField(model_attr="keywords", null=True)
     part_name = indexes.CharField(model_attr="part_name")
 
     def get_model(self):
