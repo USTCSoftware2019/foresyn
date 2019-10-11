e52ba5caafd4baa4644a861abf5abfe417d31669
myl7 <myl.ustc@gmail.com>
2019-10-11 23:33:25 +0800

    data_wrapper, share: Disable some functions to allow make migrations

--- backend/share/urls.py
+++ backend/share/urls.py
@@ -6,6 +6,6 @@ app_name = 'share'
 urlpatterns = [
     path('create', views.CreateShareLinkView.as_view(), name='create_share'),
     path('model/<int:pk>', views.ModelShareView.as_view(), name='shared_cobra_model'),
-    path('reaction/<int:pk>', views.ReactionShareView.as_view(), name='shared_cobra_reaction'),
-    path('metabolite/<int:pk>', views.MetaboliteShareView.as_view(), name='shared_cobra_metabolite'),
+    # path('reaction/<int:pk>', views.ReactionShareView.as_view(), name='shared_cobra_reaction'),
+    # path('metabolite/<int:pk>', views.MetaboliteShareView.as_view(), name='shared_cobra_metabolite'),
 ]
