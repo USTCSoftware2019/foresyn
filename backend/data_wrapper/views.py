a0ad7df9e4bdbc3ebd79cb01abea7a0c5a296cc5
biuren <biuren@mail.ustc.edu.cn>
2019-10-18 12:52:10 +0800

    data_wrapper: fixed redirect

--- backend/data_wrapper/views.py
+++ backend/data_wrapper/views.py
@@ -1,9 +1,9 @@
 import cobra
 
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from cobra_wrapper.utils import load_sbml, dump_sbml
 from bigg_database.models import Model as DataModel, Reaction as DataReaction, Metabolite as DataMetabolite
 from cobra_wrapper.models import CobraModel
@@ -21,12 +21,12 @@ class AddDataModelToCobra(View):
             model_pk = request.POST["model_bigg_id"]
         except KeyError:
             # return JsonResponse({"messages": "bigg_id required"}, status=400)
-            return redirect("")
+            return HttpResponse("bigg_id required", status=400)
         try:
             data_model_object = DataModel.objects.get(bigg_id=model_pk)
         except ObjectDoesNotExist:
             # return JsonResponse({"messages": "No such model found"}, status=404)
-            return redirect("")
+            return HttpResponse("No such model found", status=404)
         cobra_model_object = CobraModel()
         cobra_model_object.sbml_content = data_model_object.sbml_content
         cobra_model_object.owner = user
@@ -43,38 +43,33 @@ class AddDataModelToCobra(View):
 class AddDataReactionToCobra(View):
     def post(self, request):
         if not request.user.is_authenticated:
-            # return JsonResponse({"messages": "login required"}, status=401)
             return redirect("/accounts/login")
         user = request.user
         try:
             reaction_pk = request.POST["reaction_pk"]
         except KeyError:
-            # return JsonResponse({"messages": "pk required"}, status=400)
-            return redirect("")
+            return HttpResponse("reaction pk required", status=400)
         try:
             model_pk = request.POST["model_pk"]
         except KeyError:
-            # return JsonResponse({"messages": "pk required"}, status=400)
-            return redirect("")
+            return HttpResponse("model pk required", status=400)
         try:
             cobra_model_object = CobraModel.objects.get(pk=model_pk)
         except ObjectDoesNotExist:
-            # return JsonResponse({"messages": "model not found"}, status=404)
-            return redirect("")
+            return HttpResponse("No model found", status=404)
         if cobra_model_object.owner != user:
-            return redirect("")
+            return HttpResponse("Not your model", status=403)
 
         try:
             data_reaction_object = DataReaction.objects.get(pk=reaction_pk)
         except ObjectDoesNotExist:
-            # return JsonResponse({"messages": "reaction not found"}, status=404)
-            return redirect("")
+            return HttpResponse("No reaction found", status=404)
         reaction = cobra.Reaction(data_reaction_object.bigg_id)
         reaction.name = data_reaction_object.name
         metabolites_dict = reaction_string_to_metabolites(data_reaction_object.reaction_string)
 
         if metabolites_dict is None:
-            return JsonResponse({"messages": "Internal Error"}, status=500)
+            return HttpResponse("Internal Error", status=500)
         reaction.add_metabolites(metabolites_dict)
         cobra_object = load_sbml(cobra_model_object.sbml_content)
         reaction.gene_reaction_rule = [gene.gene_reaction_rule for gene in data_reaction_object.reactiongene_set.all()][
@@ -82,4 +77,4 @@ class AddDataReactionToCobra(View):
         cobra_model_object.sbml_content = dump_sbml(cobra_object)
         cobra_model_object.save()
         # return JsonResponse({"messages": "OK"}, status=200)
-        return redirect("/cobra/models/"+str(cobra_model_object.pk))
+        return redirect("/cobra/models/" + str(cobra_model_object.pk))
