c783f1bcae8bd2825a315054a849e553888c08df
myl7 <myl.ustc@gmail.com>
2019-10-22 06:50:22 +0800

    cobra_wrapper: Add RFBA

--- backend/cobra_wrapper/api_views.py
+++ backend/cobra_wrapper/api_views.py
@@ -44,64 +44,6 @@ class CobraFbaDetailJsonView(CobraComputationDetailJsonView):
     model_class = models.CobraFba
     backref_field = 'fba_list'
 
-    def get_result_dict(self):
-        result_dict = super().get_result_dict()
-        # got_compared = len(result_dict['results']) > 1
-        #
-        # def key_func(x: Dict[str, Union[str, float]]) -> str:
-        #     return x['name']
-        #
-        # result_dict['results'][0]['fluxes'].sort(key=key_func)
-        # result_dict['results'][0]['reduced_costs'].sort(key=key_func)
-        # result_dict['results'][0]['shadow_prices'].sort(key=key_func)
-        # if got_compared:
-        #     result_dict['results'][1]['fluxes'].sort(key=key_func)
-        #     result_dict['results'][1]['reduced_costs'].sort(key=key_func)
-        #     result_dict['results'][1]['shadow_prices'].sort(key=key_func)
-        #
-        # flux_name_list = [flux['name'] for flux in result_dict['results'][0]['fluxes']]
-        # shadow_price_name_list = [shadow_price['name'] for shadow_price in result_dict['results'][0]['shadow_prices']]
-        # new_result_dict = result_dict.copy()
-        # flux_pop_names = []
-        # shadow_price_pop_names = []
-        #
-        # for i, name in enumerate(flux_name_list):
-        #     if (not got_compared
-        #         and result_dict['results'][0]['fluxes'][i]['value'] < 0.001
-        #         and result_dict['results'][0]['reduced_costs'][i]['value'] < 0.001) or \
-        #             (got_compared
-        #              and result_dict['results'][0]['fluxes'][i]['value'] < 0.001
-        #              and result_dict['results'][0]['reduced_costs'][i]['value'] < 0.001
-        #              and result_dict['results'][1]['fluxes'][i]['value'] < 0.001
-        #              and result_dict['results'][1]['reduced_costs'][i]['value'] < 0.001):
-        #         flux_pop_names.append(i)
-        #
-        # for i, name in enumerate(shadow_price_name_list):
-        #     if (not got_compared
-        #         and result_dict['results'][0]['shadow_prices'][i]['value'] < 0.001) or \
-        #             (got_compared
-        #              and result_dict['results'][0]['shadow_prices'][i]['value'] < 0.001
-        #              and result_dict['results'][1]['shadow_prices'][i]['value'] < 0.001):
-        #         shadow_price_pop_names.append(i)
-        #
-        # flux_pop_names.reverse()
-        # shadow_price_pop_names.reverse()
-        #
-        # for i in flux_pop_names:
-        #     new_result_dict['results'][0]['fluxes'].pop(i)
-        #     new_result_dict['results'][0]['reduced_costs'].pop(i)
-        #     if got_compared:
-        #         new_result_dict['results'][1]['fluxes'].pop(i)
-        #         new_result_dict['results'][1]['reduced_costs'].pop(i)
-        #
-        # for i in shadow_price_pop_names:
-        #     new_result_dict['results'][0]['shadow_prices'].pop(i)
-        #     if got_compared:
-        #         new_result_dict['results'][1]['shadow_prices'].pop(i)
-        #
-        # return new_result_dict
-        return result_dict
-
     def get(self, request, *arg, **kwargs):
         result_dict = self.get_result_dict()
         for result in result_dict['results']:
@@ -113,6 +55,13 @@ class CobraFbaDetailJsonView(CobraComputationDetailJsonView):
 class CobraRgeFbaDetailJsonView(CobraComputationDetailJsonView):
     model_class = models.CobraRgeFba
     backref_field = 'rgefba_list'
+
+    def get(self, request, *arg, **kwargs):
+        result_dict = self.get_result_dict()
+        for result in result_dict['results']:
+            result['coenzymes'] = [shadow_price for shadow_price in result['shadow_prices']
+                                   if is_coenzyme(shadow_price['name'])]
+        return JsonResponse(result_dict)
 
 
 class CobraFvaDetailJsonView(CobraComputationDetailJsonView):
