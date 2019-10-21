d2c977ced1df9480845b80700d83da03f262e5e5
taoky <taoky1999@gmail.com>
2019-10-21 16:49:44 +0800

    pack: add <a> href

--- backend/accounts/templatetags/favorite_list_tag.py
+++ backend/accounts/templatetags/favorite_list_tag.py
@@ -24,3 +24,23 @@ def pack_favorite_button(fav_obj):
                              'target_name': ctp.model,
                              'target_object_id': fav_obj.target_object_id,
                              'undo': True})
+
+@register.simple_tag()
+def render_link_tag(obj):
+    if obj is None:
+        return '#'
+    if hasattr(obj, 'object'):
+        obj = obj.object
+    model = fav_obj.target_content_type.model_class()
+    if isinstance(model, Model):
+        viewname = 'bigg_database:model_detail'
+    elif isinstance(model, Reaction):
+        viewname = 'bigg_database:reaction_detail'
+    elif isinstance(model, Gene):
+        viewname = 'bigg_database:gene_detail'
+    elif isinstance(model, Metabolite):
+        viewname = 'bigg_database:metabolite_detail'
+    else:
+        return '#'
+
+    return reverse(viewname=viewname, args=(obj.target_object_id,))
