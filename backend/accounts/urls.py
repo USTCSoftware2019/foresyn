0026193e6560d162dc5b2544357a744a1ef5f4ee
taoky <taoky1999@gmail.com>
2019-10-22 10:40:12 +0800

    guide book link

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('signup/done', TemplateView.as_view(template_name="accounts/signup_done.html"), name="signup_done"),
     path('activate/<str:uidb64>/<str:token>', views.UserActivation.as_view(), name='activate'),
     path('profile/', views.UserProfile.as_view(), name='profile'),
-    path('guide/', TemplateView.as_view(template_name="accounts/guide_book.html"), name="guide"),
+    # path('guide/', TemplateView.as_view(template_name="accounts/guide_book.html"), name="guide"),
     path('pack/', views.UserPack.as_view(), name='pack')
 ]
