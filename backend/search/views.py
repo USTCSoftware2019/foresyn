e336caea4188b44b2d6332ef83eba6706771dff3
Elsa <zeyugao@outlook.com>
2019-10-26 22:58:11 +0800

    search: clean up

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,76 +1,15 @@
 from django.shortcuts import redirect, render, reverse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView, View
 from django.views.generic.edit import FormMixin
 from django.views.generic.list import MultipleObjectMixin
-from haystack.generic_views import SearchMixin
 
-from bigg_database.models import Gene as BiggGene
-from bigg_database.models import Metabolite as BiggMetabolite
-from bigg_database.models import Model as BiggModel
-from bigg_database.models import Reaction as BiggReaction
-
-from .forms import (BareSearchForm, BiggOptimizedSearchForm,
-                    ModifiedModelSearchForm)
+from .forms import BareSearchForm
 from .psql import Gene as psqlGene
 from .psql import Metabolite as psqlMetabolite
 from .psql import Model as psqlModel
 from .psql import Reaction as psqlReaction
 from .psql import count_search_result as psql_count_search_result
 from .psql import search as psql_search
-
-
-class BaseSearchView(SearchMixin, FormView):
-    form_class = ModifiedModelSearchForm
-    models = None
-
-    def get_form(self, form_class=None):
-        if form_class is None:
-            form_class = self.get_form_class()
-        return form_class(self.models, **self.get_form_kwargs())
-
-    def get(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        self.queryset = form.search()
-        context = {
-            self.form_name: form,
-            'query': form.cleaned_data['q'],
-            'object_list': self.queryset,
-            'search_model': form.search_model._meta.verbose_name,
-            'search_result_count': len(self.queryset)
-        }
-        return context
-
-
-class DatabaseSearchView(BaseSearchView):
-    form_class = BiggOptimizedSearchForm
-    models = [BiggModel, BiggReaction, BiggMetabolite, BiggGene]
-    template_name = 'bigg_database/search_result.html'
-    paginate_by = 10
-
-    def form_valid(self, form):
-        context = super().form_valid(form)
-
-        total_number = form.additional_model_count()
-        context = {
-            'total_count': sum(count for _, count in total_number.items()),
-            'counts': total_number,
-            **context
-        }
-        context = self.get_context_data(**context)
-        return self.render_to_response(context)
-
-    def form_invalid(self, form):
-        return redirect(reverse('accounts:profile'))
 
 
 class BiGGDatabaseSearchView(MultipleObjectMixin, FormView):
